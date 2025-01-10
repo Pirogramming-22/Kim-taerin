ed95f33c4f1ecff8f3573ad1d71b2a4b853545e5
ttaerrin <redgreenyell@naver.com>
2025-01-10 17:20:26 +0900

    readme 작성 및 상단바 수정

--- myMovieReviews/review/models.py
+++ myMovieReviews/review/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class MovieReview(models.Model):
     title = models.CharField(max_length=100)
     director = models.CharField(max_length=50)
+    cast = models.CharField(max_length=200, blank=True)
     genre = models.CharField(max_length=30)
     release_year = models.IntegerField()
     rating = models.FloatField()
     runtime = models.IntegerField()
     content = models.TextField()
-    cast = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
